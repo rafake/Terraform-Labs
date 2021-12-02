958db8577adb1a44409ae4c01c865f9728242248
rafake <kelmrafal@gmail.com>
2021-12-02 02:13:41 -0800

    poprawka

---
+++ Zadanie/variables copy.tf
@@ -0,0 +1,30 @@
+variable "sub-id" {
+  type = string
+  #TODO: mifurm, zmieniam na SUB-07
+  default = "53d60844-4bdb-410f-a330-2fa78aa5ca81"
+}
+
+# variable "rg-name" {
+#   type = string
+#   default = "zadanie"
+# }
+
+# variable "app-serv-name" {
+#     type = string
+#     default = "aps-mf-dev-01"
+# }
+
+# variable "key-vault-name" {
+#     type = string
+#     default = "kv-mf-dev-01"
+# }
+
+# variable "function-storage-name" {
+#     type = string
+#     default = "samffundev01"
+# }
+
+# variable "aks-cluster-name" {
+#   type = string
+#   default = "aks-mf-dev-01"
+# }
